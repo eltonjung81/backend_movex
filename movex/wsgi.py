fad1c4aad6f44ef3b2efee8f719c84c76e9a29e8
eltonjung81 <eltonjung81@gmail.com>
2025-04-10 10:27:16 -0300

    Run create_superuser.py automatically during deploy

--- movex/wsgi.py
+++ movex/wsgi.py
@@ -3,6 +3,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 import django
 from django.core.management import call_command
+import subprocess
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movex.settings')
 
@@ -12,6 +13,12 @@ try:
     call_command('migrate')
 except Exception as e:
     print(f"Erro ao aplicar migrações: {e}")
+
+# Executar o script para criar superusuário e diretor automaticamente
+try:
+    subprocess.run(['python', 'create_superuser.py'], check=True)
+except Exception as e:
+    print(f"Erro ao executar o script create_superuser.py: {e}")
 
 # Configuração do WSGI com WhiteNoise
 application = get_wsgi_application()
