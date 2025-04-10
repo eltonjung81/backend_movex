df16f0f5044c48e36414f4d9bbbe778e1c4af37a
eltonjung81 <eltonjung81@gmail.com>
2025-04-10 10:16:41 -0300

    Run migrations automatically on server start

--- movex/wsgi.py
+++ movex/wsgi.py
@@ -1,8 +1,17 @@
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+import django
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movex.settings')
+
+# Rodar migrações automaticamente ao iniciar o servidor
+try:
+    django.setup()
+    call_command('migrate')
+except Exception as e:
+    print(f"Erro ao aplicar migrações: {e}")
 
 # Configuração do WSGI com WhiteNoise
 application = get_wsgi_application()
